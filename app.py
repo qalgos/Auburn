3dfb253d5dd254aa6b85adbca85ac6bafa7c87d6
Adam Wesołowski <123480804+qalgos@users.noreply.github.com>
2025-11-06 16:34:47 +0000

    Update app.py

--- app.py
+++ app.py
@@ -16,11 +16,12 @@ logo = Image.open("image0.jpeg")
 # Create columns for logo and title
 col1, col2 = st.columns([1, 4])
 with col1:
-    st.image(logo, width=120)  # Adjust width as needed
+    st.image(logo, width=140)  # Adjust width as needed
 with col2:
+    st.markdown('<h1 class="main-header">Auburn</h1>', unsafe_allow_html=True)
    st.set_page_config(
         page_title="Auburn",
-        page_icon="🧬",
+        page_icon= logo,
         layout="wide",
         initial_sidebar_state="expanded"
     )
@@ -337,12 +338,8 @@ if authenticate():
 
     # ABOUT PAGE
     if page == "About":
-        # Create columns for logo and title
-        col1, col2 = st.columns([1, 4])
-        with col1:
-            st.image(logo, width=80)  # Adjust width as needed
-        with col2:
-            st.markdown('<h1 class="main-header">Auburn</h1>', unsafe_allow_html=True)
+        
+        
         
         col1, col2 = st.columns([2, 1])
         
