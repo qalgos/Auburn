9b4c91f1aa7c2fd30bdd4e7d1d4bd88050fea153
Adam Wesołowski <123480804+qalgos@users.noreply.github.com>
2025-11-06 16:36:07 +0000

    Update app.py

--- app.py
+++ app.py
@@ -17,8 +17,9 @@ logo = Image.open("image0.jpeg")
 col1, col2 = st.columns([1, 4])
 with col1:
     st.image(logo, width=140)  # Adjust width as needed
-with col2:
     st.markdown('<h1 class="main-header">Auburn</h1>', unsafe_allow_html=True)
+with col2:
+    
     st.set_page_config(
         page_title="Auburn",
         page_icon= logo,
